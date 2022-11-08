413a6cd02d1f280a8638130d0e980754750fde6e
André Bernardes <andreb07@gmail.com>
2022-11-08 23:08:04 +0000

    Update

--- src/App.vue
+++ src/App.vue
@@ -56,7 +56,7 @@ export default {
         { property: "og:title", content: () => this.AppData.translations.site_title },
         { property: "og:url", content: () => this.AppUrl },
         { property: "og:description", content: () => this.AppData.translations.site_description },
-        { property: "og:image", content: () => this.AppData.translations.share_image_facebook },
+        { property: "og:image:secure", content: () => this.AppData.translations.share_image_facebook },
         { name: 'twitter:title',content: () => this.AppData.translations.site_title },
         { name: 'twitter:description',content: () => this.AppData.translations.site_description + ' - ' + this.AppUrl },
         { name: 'twitter:image',content: () => this.AppData.translations.share_image_twitter },
